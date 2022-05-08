822f2e67cefd17ddf4126a0931c2e633ec7703b1
Benjamin Mirisola <bennymirisola@gmail.com>
2022-05-08 13:30:30 -0400

    moved recognizer out of the function

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import speech_recognition as sr
 
+r = sr.Recognizer()
 def get_audio():
+
     while True:
-        r = sr.Recognizer()
         with sr.Microphone() as source:
             print("Listening")
             audio = r.listen(source)
