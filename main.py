e5d12b374b91933ba7aadfc900fbb3fd0b73dd6d
Benjamin Mirisola <bennymirisola@gmail.com>
2022-05-08 13:39:12 -0400

    hears robert

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ def get_audio():
             try:
                 said = r.recognize_google(audio, language="en-US")
                 print(said)
+                if(said.__contains__("Robert")):
+                   print("heard")
             except Exception as e:
                 print(e)
+
+def lightSwitch(state):
+    pass
 
 if __name__ == "__main__":
     get_audio()
