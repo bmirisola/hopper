03a34d9e241023fbf2fe6f1165aedd74776f56be
Benjamin Mirisola <bennymirisola@gmail.com>
2022-05-11 02:12:08 -0400

    added ding file and audio

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import speech_recognition as sr
-
+from playsound import playsound
+import os
 r = sr.Recognizer()
+ding = os.getcwd() + '/ding.mp3'
 def get_audio():
 
     while True:
         with sr.Microphone() as source:
             print("Listening")
-            audio = r.listen(source, timeout=1.5)
             try:
+                audio = r.listen(source, timeout=2, phrase_time_limit= 1.5)
                 said = r.recognize_google(audio, language="en-US")
                 print(said)
                 if(said.__contains__("Robert")):
-                   print("heard")
-            except Exception as e:
-                print(e)
+                    print(ding)
+            except sr.WaitTimeoutError as e:
+                pass
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
 
 def lightSwitch(state):
     pass
 
 if __name__ == "__main__":
-    get_audio()
+    #get_audio()
+    playsound(ding)
