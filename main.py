3ed4b669d0e8b103ee97d79dcb6bea386834f2af
Benjamin Mirisola <bennymirisola@gmail.com>
2022-05-20 14:39:07 -0400

    added enums to servo call

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import speech_recognition as sr
 from playsound import playsound
+import servo
 import os
 r = sr.Recognizer()
 ding = os.getcwd() + '/ding.mp3'
+
 def get_audio():
 
     while True:
@@ -19,10 +21,10 @@ def get_audio():
                         lights_audio = r.listen(source, timeout=10, phrase_time_limit=6)
                         lights_said = r.recognize_google(lights_audio, language="en-US")
                         if (lights_said.__contains__('on')):
-                            print('on')
+                            lightSwitch(servo.LIGHT_SWITCH_STATES.ON)
 
                         elif (lights_said.__contains__('off')):
-                            print('off')
+                            lightSwitch(servo.LIGHT_SWITCH_STATES.OFF)
                     except sr.WaitTimeoutError as e:
                         pass
                     except sr.UnknownValueError:
@@ -34,7 +36,11 @@ def get_audio():
                 print("Google Speech Recognition could not understand audio")
 
 def lightSwitch(state):
-    pass
+    if(state == servo.LIGHT_SWITCH_STATES.OFF):
+        print('off')
+
+    elif(state == servo.LIGHT_SWITCH_STATES.ON):
+        print('on')
 
 if __name__ == "__main__":
     get_audio()
---
+++ servo.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class LIGHT_SWITCH_STATES(Enum):
+    OFF = 0
+    ON = 1
+
